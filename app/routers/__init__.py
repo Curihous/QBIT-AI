c77c28201909a9392244d2e8a7129a7238a2b809
goblurry <offmymind123@gmail.com>
2025-11-17 00:58:06 +0900

    feat: 이론학습 카드 API 엔드포인트 추가

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,6 @@
 from .report import router as report_router
 from .news import router as news_router
+from .learning import router as learning_router
 
-__all__ = ["report_router", "news_router"]
+__all__ = ["report_router", "news_router", "learning_router"]
 
