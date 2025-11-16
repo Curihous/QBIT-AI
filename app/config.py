a0daa8e19024c6e476b741c3774075406e7e4ebf
goblurry <offmymind123@gmail.com>
2025-11-16 19:50:05 +0900

    Bump version to 1.2.2

--- app/config.py
+++ app/config.py
@@ -40,7 +40,7 @@ class Settings(BaseSettings):
 
     # 애플리케이션 버전
     # deploy브랜치로 병합할 때
-    app_version: str = "1.2.1"
+    app_version: str = "1.2.2"
 
     @property
     def allowed_origins_list(self) -> list[str]:
